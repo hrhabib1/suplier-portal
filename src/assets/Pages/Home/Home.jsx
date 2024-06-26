748658720d9dfa9b490fd5318e529476e1aae023
hrhabib1 <sayefhabib123@gmail.com>
2024-06-26 13:17:31 +0600

    front-end develope

--- src/assets/Pages/Home/Home.jsx
+++ src/assets/Pages/Home/Home.jsx
@@ -1,8 +1,7 @@
 const Home = () => {
     return (
         <div>
-            <h1>This is home ....................................................................... Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque et modi optio! Blanditiis qui hic accusamus alias. Tempore rerum possimus fugiat pariatur. Neque, pariatur qui! Quod nobis dolorum quia, animi recusandae dignissimos maxime explicabo, cumque esse voluptate excepturi, eos aperiam temporibus inventore assumenda soluta odio aut? Laudantium omnis dolore quasi. Nulla reprehenderit pariatur deserunt aspernatur beatae ex, fugit error voluptas suscipit. Adipisci dolor consequatur natus perferendis. Repellat dolor est, quod modi totam ad autem omnis explicabo natus magni similique tenetur hic non, quisquam, illo culpa repudiandae sit. Atque, voluptatem enim quo cumque vitae eveniet! Quisquam, pariatur nobis? Eos, inventore voluptatum!</h1>
-        </div>
+         </div>
     );
 };
 
